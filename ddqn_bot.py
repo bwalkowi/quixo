85fa011d644c15e6a6345542c946654387b2293b
Bartosz Walkowicz <wallko1@hotmail.com>
2019-04-29 21:02:09 +0200

    fix ddqn implementation

--- ddqn_bot.py
+++ ddqn_bot.py
@@ -4,10 +4,11 @@ from typing import Tuple, Optional
 from random import random, choice, sample
 
 import numpy as np
+from numpy.ma import masked_array
 import tensorflow as tf
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.optimizers import Adam
+from keras.optimizers import RMSprop
 
 from utils import (Mark, Action, Result, ALL_MOVES,
                    get_possible_moves, get_encoded_possible_moves,
@@ -17,7 +18,8 @@ from utils import (Mark, Action, Result, ALL_MOVES,
 class Player:
     def __init__(self, mark: Mark, *,
                  training: bool = False,
-                 batch_size: int = 32,
+                 batch_size: int = 64,
+                 max_tau: int = 250,
                  gamma: float = 0.95,
                  epsilon: float = 1.0,
                  epsilon_min: float = 0.1,
@@ -28,14 +30,17 @@ class Player:
         self.training = training
         self.batch_size = batch_size
 
+        self.tau = 1
+        self.max_tau = max_tau
+
         self.gamma = gamma
         self.epsilon = epsilon
         self.epsilon_min = epsilon_min
         self.epsilon_decay = epsilon_decay
 
-        self.last_board = None
-        self.last_move = None
-        self.memory = deque(maxlen=2000)
+        self.prev_board = None
+        self.prev_move = None
+        self.memory = deque(maxlen=10_000)
 
         self.model = build_dqn(learning_rate)
         self.target_model = build_dqn(learning_rate)
@@ -60,39 +65,60 @@ class Player:
             chosen_move = ALL_MOVES[move_hash]
 
         if self.training:
-            if self.last_board:
-                last_state = encode_board(self.last_board, self.mark, as_batch=False)
-                self.memory.append((last_state, self.last_move, 0, board, False))
+            self.tau += 1
+            if self.tau > self.max_tau:
+                self.tau = 0
+                self.update_target_model()
 
-            self.last_board = board
-            self.last_move = move_hash
+            self.memorize(board, move_hash, 0, False)
 
         return chosen_move
 
     def train(self, result: Result) -> None:
-        last_state = encode_board(self.last_board, self.mark, as_batch=False)
-        self.memory.append((last_state, self.last_move, result.value, None, True))
-
-        self.last_move = None
-        self.last_board = None
+        self.memorize(None, None, result.value, True)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
         if len(self.memory) > 1000:
+            rows = np.arange(self.batch_size)
             batch = sample(self.memory, self.batch_size)
 
-            states = np.array([state for state, *_ in batch])
-            targets = self.model.predict(states)
-            for i, (_, move_hash, reward, next_board, done) in enumerate(batch):
-                if done:
-                    targets[i, move_hash] = reward
-                else:
-                    next_state = encode_board(next_board, self.mark)
-                    x = self.target_model.predict(next_state)[0]
-                    x_valid = mask_invalid_moves(next_board, self.mark, x)
-                    targets[i, move_hash] = reward + self.gamma * np.amax(x_valid)
+            states, moves, rewards, next_states, masks, done = zip(*batch)
 
-            self.model.fit(states, targets, epochs=1, verbose=0)
+            states = np.array(states)
+            q_targets = self.model.predict(states)
+
+            next_states = np.array(next_states)
+            next_qs_model = masked_array(self.model.predict(next_states),
+                                         mask=np.array(masks))
+            next_qs_target = self.target_model.predict(next_states)
+            next_qs_values = next_qs_target[rows, np.argmax(next_qs_model, axis=1)]
+
+            rewards = np.array(rewards)
+            done = np.array(done, dtype=np.int32)
+
+            q_targets[rows, moves] = rewards + self.gamma * next_qs_values * (1 - done)
+
+            self.model.fit(states, q_targets, epochs=1, verbose=0)
+
+    def memorize(self, board, move_hash, reward, done):
+        if self.prev_board:
+            prev_state = encode_board(self.prev_board, self.mark,
+                                      as_batch=False)
+            if done:
+                invalid_moves_mask = [0] * MOVE_SPACE_SIZE
+                curr_state = prev_state
+            else:
+                opponent_mark = self.mark.opposite_mark()
+                invalid_moves_mask = [board[row][col] is opponent_mark
+                                      for row, col, _ in ALL_MOVES]
+                curr_state = encode_board(board, self.mark, as_batch=False)
+
+            self.memory.append((prev_state, self.prev_move, reward,
+                                curr_state, invalid_moves_mask, done))
+
+        self.prev_board = board
+        self.prev_move = move_hash
 
     def update_target_model(self):
         self.target_model.set_weights(self.model.get_weights())
@@ -112,16 +138,10 @@ def build_dqn(learning_rate: float = 0.001) -> Sequential:
         Dense(128, activation='relu'),
         Dense(MOVE_SPACE_SIZE, activation='linear'),
     ])
-    model.compile(optimizer=Adam(lr=learning_rate),
+    model.compile(optimizer=RMSprop(lr=learning_rate),
                   loss=tf.losses.huber_loss)
     return model
 
 
 def sort_predictions(predictions):
     return sorted(zip(predictions, range(MOVE_SPACE_SIZE)), reverse=True)
-
-
-def mask_invalid_moves(board, mark, predictions):
-    opponent_mark = mark.opposite_mark()
-    mask = [board[row][col] is opponent_mark for row, col, _ in ALL_MOVES]
-    return np.ma.masked_array(predictions, mask=mask)
